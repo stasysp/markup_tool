9bad615c6768b751584b8f87270bf6f70d3e468c
RomanN91 <nagornov.r@gmail.com>
2019-03-21 22:00:28 +0300

    Basic project structure

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "markup_backend/pipeline.h"
+
+int main() {
+    run_pipeline();
+    return 0;
+}
+
---
+++ markup_backend/src/pipeline.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "markup_backend/pipeline.h"
+
+using namespace std;
+
+void run_pipeline() {
+    cout << "Hello Pipeline!" << endl;
+}
