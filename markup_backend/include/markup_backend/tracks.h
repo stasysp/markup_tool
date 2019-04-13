5ff774f89d1000270ce6b21c716843f5280e1f50
RomanN91 <nagornov.r@gmail.com>
2019-04-13 16:04:54 +0300

    Refactored track.h

--- markup_backend/include/markup_backend/tracks.h
+++ markup_backend/include/markup_backend/tracks.h
@@ -12,40 +12,17 @@ class Track {
 public:
     Track() = delete;
     explicit Track(size_t id) : id_(id) {}
-    void push_back(Detection det) {
-        assert(det.id == id_);
 
-        detections_.push_back(det);
-    }
+    void push_back(Detection det);
+    size_t get_id() const;
 
-    size_t get_id() const {
-        return id_;
-    }
+    std::list<Detection>::iterator begin();
+    std::list<Detection>::iterator end();
 
-    std::list<Detection>::iterator begin() {
-        return detections_.begin();
-    }
+    std::list<Detection>::const_iterator begin() const;
+    std::list<Detection>::const_iterator end() const;
 
-    std::list<Detection>::iterator end() {
-        return detections_.end();
-    }
-
-    std::list<Detection>::const_iterator begin() const {
-        return detections_.begin();
-    }
-
-    std::list<Detection>::const_iterator end() const {
-        return detections_.end();
-    }
-
-    std::unique_ptr<Detection> get_detection(size_t frame_idx) {
-        for (const auto& detection : detections_) {
-            if (detection.frame == frame_idx) {
-                return std::make_unique<Detection>(detection);
-            }
-        }
-        return nullptr;
-    }
+    std::unique_ptr<Detection> get_detection(size_t frame_idx) ;
 
 private:
     std::list<Detection> detections_;
@@ -68,57 +45,18 @@ class TrackContainer {
 public:
     TrackContainer() = delete;
 
-    // TrackContainer() = default;
-    explicit TrackContainer(size_t video_length) {
-        this->video_len_ = video_length;
-        timeline_.resize(this->video_len_);
-    }
+    explicit TrackContainer(size_t video_length);
 
-    void add_track(const Track& track) {
-        // Checks
-        tracks_.insert(track);
-        for (auto& saved_track : tracks_) {
-            if (saved_track.get_id() == track.get_id()) {
-                for (auto &detection : saved_track) {
-                    assert(detection.frame < video_len_);
-                    timeline_[detection.frame].insert(&detection);
-                }
-                break;
-            }
-        }
-    }
+    void add_track(const Track& track);
 
-    std::unique_ptr<Track> get_track(size_t id) const {
-        for (const auto& track : tracks_) {
-            if (track.get_id() == id) {
-                return std::make_unique<Track>(track);
-            }
-        }
+    std::unique_ptr<Track> get_track(size_t id) const;
 
-        return nullptr;
-    }
+    std::vector<Detection> get_detections(size_t frame_idx) const;
 
-    std::vector<Detection> get_detections(size_t frame_idx) const {
-        assert(frame_idx < video_len_);
+    size_t get_video_len() const;
+    size_t get_num_tracks() const;
 
-        std::vector<Detection> detections;
-        for (const auto& detection_ptr : timeline_[frame_idx]) {
-            detections.push_back(*detection_ptr);
-        }
-        return detections;
-    }
-
-    size_t get_video_len() const {
-        return video_len_;
-    }
-
-    size_t get_num_tracks() const {
-        return tracks_.size();
-    }
-
-    bool empty() const {
-        return tracks_.size() == 0;
-    }
+    bool empty() const;
 
     // void add_track(Track);
     // get all detections at frame_index
@@ -135,40 +73,5 @@ private:
     std::set<Track, TrackComparator> tracks_;
     std::vector<std::set<const Detection*, DetectionComparator>> timeline_;
 };
-
-/*class DummyTrackContainer {
-public:
-    // TrackContainer() = delete;
-    DummyTrackContainer() {
-
-    }
-
-    void add_track(const Track& track) {
-
-    }
-
-    std::unique_ptr<Track> get_track(size_t id) const {
-        for (const auto& track : tracks_) {
-            if (track.get_id() == id) {
-                return std::make_unique<Track>(track);
-            }
-        }
-
-        return nullptr;
-    }
-
-    //std::vector<Detection> get_detections(size_t frame_idx) {
-    //    assert(frame_idx < video_len_);
-    // }
-
-    size_t get_video_len() const {
-        return video_len_;
-    }
-private:
-    size_t video_len_;
-    std::vector<Track> tracks_;
-};*/
-
-
 
 TrackContainer make_dummy_track();
--- markup_backend/src/tracks.cpp
+++ markup_backend/src/tracks.cpp
@@ -4,5 +4,93 @@ TrackContainer make_dummy_track() {
     return TrackContainer(5);
 }
 
+// ---------------- class Track --------------------------
+
+void Track::push_back(Detection det) {
+    assert(det.id == id_);
+
+    detections_.push_back(det);
+}
+
+size_t Track::get_id() const {
+    return id_;
+}
+
+std::list<Detection>::iterator Track::begin() {
+    return detections_.begin();
+}
+
+std::list<Detection>::iterator Track::end() {
+    return detections_.end();
+}
+
+std::list<Detection>::const_iterator Track::begin() const {
+    return detections_.begin();
+}
+
+std::list<Detection>::const_iterator Track::end() const {
+    return detections_.end();
+}
+
+std::unique_ptr<Detection> Track::get_detection(size_t frame_idx) {
+    for (const auto& detection : detections_) {
+        if (detection.frame == frame_idx) {
+            return std::make_unique<Detection>(detection);
+        }
+    }
+    return nullptr;
+}
+
 // ---------------- class TrackContainer -----------------
 
+TrackContainer::TrackContainer(size_t video_length) {
+    this->video_len_ = video_length;
+    timeline_.resize(this->video_len_);
+}
+
+void TrackContainer::add_track(const Track& track) {
+    // Checks
+    tracks_.insert(track);
+    for (auto& saved_track : tracks_) {
+        if (saved_track.get_id() == track.get_id()) {
+            for (auto &detection : saved_track) {
+                assert(detection.frame < video_len_);
+                timeline_[detection.frame].insert(&detection);
+            }
+            break;
+        }
+    }
+}
+
+std::unique_ptr<Track> TrackContainer::get_track(size_t id) const {
+
+    for (const auto& track : tracks_) {
+        if (track.get_id() == id) {
+            return std::make_unique<Track>(track);
+        }
+    }
+
+    return nullptr;
+}
+
+std::vector<Detection> TrackContainer::get_detections(size_t frame_idx) const {
+    assert(frame_idx < video_len_);
+
+    std::vector<Detection> detections;
+    for (const auto& detection_ptr : timeline_[frame_idx]) {
+        detections.push_back(*detection_ptr);
+    }
+    return detections;
+}
+
+size_t TrackContainer::get_video_len() const {
+    return video_len_;
+}
+
+size_t TrackContainer::get_num_tracks() const {
+    return tracks_.size();
+}
+
+bool TrackContainer::empty() const {
+    return tracks_.size() == 0;
+}
